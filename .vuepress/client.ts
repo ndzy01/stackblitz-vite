a486439ff04fbe6fea832b8dec312dc008045fcf
ndzy01 <2301173216@qq.com>
2022-08-07 09:43:57 +0800

    2 files modified, 1 added

--- .vuepress/client.ts
+++ .vuepress/client.ts
@@ -1,23 +1,21 @@
 import { defineClientConfig } from '@vuepress/client';
 import './index.css';
-import Pwd from './components/pwd.vue';
+// import Pwd from './components/pwd.vue';
 
 export default defineClientConfig({
   enhance({ app, router }) {
-    app.component('n-pwd', Pwd);
-
-    router.beforeEach((to, from, next) => {
-      const flag = globalThis.sessionStorage?.getItem('flag') || '';
-
-      if (to.path !== '/') {
-        if (flag === 'ndzy') {
-          next();
-        } else {
-          next({ path: '/' });
-        }
-      } else {
-        next();
-      }
-    });
+    // app.component('n-pwd', Pwd);
+    // router.beforeEach((to, from, next) => {
+    //   const flag = globalThis.sessionStorage?.getItem('flag') || '';
+    //   if (to.path !== '/') {
+    //     if (flag === 'ndzy') {
+    //       next();
+    //     } else {
+    //       next({ path: '/' });
+    //     }
+    //   } else {
+    //     next();
+    //   }
+    // });
   },
 });
